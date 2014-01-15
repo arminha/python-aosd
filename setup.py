e1af2b5bd98ecc69f6fa81efd11ba01284279bd0
Armin Häberling <armin.aha@gmail.com>
2014-01-15 20:08:07 +0100

    Add Python 3 classifier.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup (
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-#        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3',
         'Intended Audience :: Developers',
         'Topic :: Desktop Environment',
         'Topic :: Software Development :: Libraries',
